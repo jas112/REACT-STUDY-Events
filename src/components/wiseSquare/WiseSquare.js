3560c7a707c91b6882081b13b49933d669b8f17e
jas112 <jas.sandmaster@gmail.com>
2022-07-13 20:44:11 -0700

    started AnnoyingForm and CopyDemo components.

--- src/components/wiseSquare/WiseSquare.js
+++ src/components/wiseSquare/WiseSquare.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import './WiseSquare.css';
 
 class WiseSquare extends Component {
-    dispenseWisdom(){
+    constructor(props){
+        super(props);
+        this.state = {
+            message: ''
+        };
+        this.dispenseWisdom = this.dispenseWisdom.bind(this);
+    }
+
+    dispenseWisdom(e){
         let messages = [
             "Your eyes can deceive you; don't trust them.",
             "Who's more foolish? The fool or the fool who follows him?",
@@ -11,13 +19,32 @@ class WiseSquare extends Component {
             "Difficult to see. Always in motion is the future."
         ];
 
-        let rIdx = Math.floor(Math.random()* messages.length);
+        let rIdx;
+        let isRepeatedValue = false;
+
+        do {
+            rIdx = Math.floor(Math.random()* messages.length);
+            isRepeatedValue = (messages[rIdx] === this.state.message);
+            console.log(`isRepeatedValue: ${isRepeatedValue}`);
+        } while (isRepeatedValue);
+
         console.log(messages[rIdx]);
+
+        this.setState(currState => {
+            let newState = {...currState};
+            newState.message = messages[rIdx];
+            return newState;
+        });
+
     }
 
   render() {
     return (
-      <div className='WiseSquare' onMouseEnter={this.dispenseWisdom}>😎</div>
+        <div>
+            <div className='WiseSquare' onMouseEnter={this.dispenseWisdom}>🧐</div>
+            <h2>{this.state.message}</h2>
+        </div>
+      
     )
   }
 }
